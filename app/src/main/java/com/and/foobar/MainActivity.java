d3ba6c34f91ba00162ef7dd33c3994c2fdbb2021
fathin fadhil <fadhx7@gmail.com>
2022-04-01 03:56:17 +0700

    commit pertama

---
+++ app/src/main/java/com/and/foobar/MainActivity.java
@@ -0,0 +1,22 @@
+package com.and.foobar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragmentcontainer, FragmentLogin.class, null)
+                .commit();
+    }
+}
