5a6e684fabbbbb14cb75496491fd64342b340943
fathin fadhil <fadhx7@gmail.com>
2022-04-01 22:50:45 +0700

    menambahkan click listener pada tombol dan textview signup

--- app/src/main/java/com/and/foobar/FragmentLogin.java
+++ app/src/main/java/com/and/foobar/FragmentLogin.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.text.MessageFormat;
 
 public class FragmentLogin extends Fragment {
     @Nullable
@@ -15,6 +21,21 @@ public class FragmentLogin extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        Button btnLogin = v.findViewById(R.id.login_button);
+        EditText emailText = v.findViewById(R.id.editTextTextEmailAddress);
+        EditText passText = v.findViewById(R.id.editTextTextPassword);
+        TextView signUp = v.findViewById(R.id.signup);
+
+        btnLogin.setOnClickListener(view ->
+                Toast.makeText(requireActivity().getApplicationContext(),
+                        MessageFormat.format("Email = {0} dan Pass = {1}", emailText.getText(), passText.getText()),
+                        Toast.LENGTH_SHORT).show());
+
+        signUp.setOnClickListener(view ->
+                Toast.makeText(requireActivity().getApplicationContext(),
+                    "Sign up terclick!",
+                    Toast.LENGTH_SHORT).show());
+        return v;
     }
 }
